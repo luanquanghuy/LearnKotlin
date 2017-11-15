793190b477fdf2c98fcb327b4658a1142196961c
Quang Huy <luanquanghuy@gmail.com>
2017-11-15 14:07:30 +0700

    cập nhật phân số

--- src/PhanSo.kt
+++ src/PhanSo.kt
@@ -1,9 +1,14 @@
 class PhanSo {
-    private var tuSo : Int = 0
-    private var mauSo : Int = 0
-
-    constructor()
+//    numerator
+//    denominator
+    var tuSo : Int = 0
+    var mauSo : Int = 0
+    constructor(){
+        this.tuSo = 1
+        this.mauSo = 1
+    }
     constructor(tuSo: Int, mauSo : Int){
+        if (mauSo == 0) throw IllegalArgumentException("Mẫu số phải khác 0")
         this.tuSo = tuSo
         this.mauSo = mauSo
     }
@@ -29,39 +34,40 @@ class PhanSo {
         else println("$tuSo/$mauSo")
     }
 
-    fun tong(ps1:PhanSo):PhanSo{
+    fun tong(phanSo:PhanSo):PhanSo{
         var ps:PhanSo = PhanSo()
-        ps.tuSo = tuSo*ps1.mauSo+ps1.tuSo*mauSo
-        ps.mauSo = mauSo*ps1.mauSo
+        ps.tuSo = tuSo*phanSo.mauSo+phanSo.tuSo*mauSo
+        ps.mauSo = mauSo*phanSo.mauSo
         ps.toiGian()
         return ps
     }
 
-    fun hieu(ps1: PhanSo):PhanSo{
+    fun hieu(phanSo: PhanSo):PhanSo{
         var ps:PhanSo = PhanSo()
-        ps.tuSo = tuSo*ps1.mauSo-ps1.tuSo*mauSo
-        ps.mauSo = mauSo*ps1.mauSo
+        ps.tuSo = tuSo*phanSo.mauSo-phanSo.tuSo*mauSo
+        ps.mauSo = mauSo*phanSo.mauSo
         ps.toiGian()
         return ps
     }
 
-    fun tich(ps1: PhanSo):PhanSo{
+    fun tich(phanSo : PhanSo):PhanSo{
         var ps:PhanSo = PhanSo()
-        ps.tuSo = tuSo*ps1.tuSo
-        ps.mauSo = mauSo*ps1.mauSo
+        ps.tuSo = tuSo*phanSo.tuSo
+        ps.mauSo = mauSo*phanSo.mauSo
         ps.toiGian()
         return ps
     }
 
-    fun thuong(ps1: PhanSo):PhanSo{
+    fun thuong(phanSo: PhanSo):PhanSo{
         var ps:PhanSo = PhanSo()
-        ps.tuSo = tuSo*ps1.mauSo
-        ps.mauSo = mauSo*ps1.tuSo
+        ps.tuSo = tuSo*phanSo.mauSo
+        ps.mauSo = mauSo*phanSo.tuSo
         ps.toiGian()
         return ps
     }
 
     fun nghichDao():PhanSo{
+        if(this.tuSo == 0) throw IllegalArgumentException("Tử số khác 0 khi nghịch đảo")
         var ps = PhanSo(tuSo, mauSo)
         ps.tuSo = ps.mauSo.also { ps.mauSo = ps.tuSo }
         return ps
--- src/TinhToanPhanSo.kt
+++ src/TinhToanPhanSo.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
-    var ps1 : PhanSo = PhanSo(9, 6)
+    var ps1 : PhanSo = PhanSo(0, 4)
     var ps2 : PhanSo = PhanSo(2, 4)
-    var ps3 = ps1.tich(ps2)
+    ps1.tuSo = 3
+    var ps3 = ps1.tong(ps2)
     ps3.inGiaTri()
 }
